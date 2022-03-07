b066623995b1992c13cf64f6cfa75ec27c86a265
Ori Dabush <dabushori@gmail.com>
2022-03-07 13:26:37 +0200

    added mining time logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,29 @@ public class Player : MonoBehaviour
     float MINING_DISTANCE = 2f;
     int DISTRUCTABLE_LAYER = 1 << 7;
 
+    bool isPressed = false;
     DateTime lastClick;
-    TimeSpan delay = TimeSpan.FromSeconds(1);
+    DateTime lastUnclick;
+    TimeSpan miningTime = TimeSpan.FromSeconds(1);
+
+    private void UpdatePressed()
+    {
+        if (!isPressed && Pointer.current.press.isPressed)
+        {
+            isPressed = true;
+            lastClick = DateTime.Now;
+        }
+        else if (isPressed && !Pointer.current.press.isPressed)
+        {
+            isPressed = false;
+            lastUnclick = DateTime.Now;
+        }
+    }
 
     private void Update()
     {
-        if (Pointer.current.press.isPressed && (lastClick + delay) < DateTime.Now)
+        UpdatePressed();
+        if (Pointer.current.press.isPressed && (lastClick + miningTime) < DateTime.Now)
         {
             lastClick = DateTime.Now;
             Vector3 pos = gameObject.transform.position;
