4feda64ea0e7a3e8e0e7b98b43eef866e9cc3d99
Ori Dabush <dabushori@gmail.com>
2022-04-06 17:34:16 +0300

    Implemented the basic mob (agent) prefab

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -5,6 +5,7 @@ public  class Item : ScriptableObject
 {
     new public string name = "New Item"; // item name
     public int breakDamage = 50; // damage done to destructible objects
+    public int hitDamage = 50; // damage done to mobs
     public bool usable = true; // can be used in the world (eat / build / something)
     public Sprite icon = null; // path to image 
 }
