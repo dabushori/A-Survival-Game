ea85f9695302269a511c5535b2a4a33ee32f325f
Ori Dabush <dabushori@gmail.com>
2022-04-19 15:46:55 +0300

    optimized the random items generation algorithm

--- Assets/Scripts/WorldGeneration/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration/WorldGeneration.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -20,21 +18,6 @@ public class WorldGeneration : MonoBehaviour
     [SerializeField]
     private NoiseMapGeneration.Wave[] waves;
     
-    public class TileCoordinate
-    {
-        public int tileZIndex;
-        public int tileXIndex;
-        public int coordinateZIndex;
-        public int coordinateXIndex;
-        public TileCoordinate(int tileZIndex, int tileXIndex, int coordinateZIndex, int coordinateXIndex)
-        {
-            this.tileZIndex = tileZIndex;
-            this.tileXIndex = tileXIndex;
-            this.coordinateZIndex = coordinateZIndex;
-            this.coordinateXIndex = coordinateXIndex;
-        }
-    }
-
     public class WorldData
     {
         private int tileDepthInVertices, tileWidthInVertices;
@@ -50,19 +33,6 @@ public class WorldGeneration : MonoBehaviour
         {
             // save the TileData in the corresponding coordinate
             tilesData[tileZIndex, tileXIndex] = tileData;
-        }
-
-        public TileCoordinate ConvertToTileCoordinate(int zIndex, int xIndex)
-        {
-            // the tile index is calculated by dividing the index by the number of tiles in that axis
-            int tileZIndex = (int)Mathf.Floor((float)zIndex / (float)tileDepthInVertices);
-            int tileXIndex = (int)Mathf.Floor((float)xIndex / (float)tileWidthInVertices);
-            // the coordinate index is calculated by getting the remainder of the division above
-            // we also need to translate the origin to the bottom left corner
-            int coordinateZIndex = tileDepthInVertices - (zIndex % tileDepthInVertices) - 1;
-            int coordinateXIndex = tileWidthInVertices - (xIndex % tileDepthInVertices) - 1;
-            TileCoordinate tileCoordinate = new TileCoordinate(tileZIndex, tileXIndex, coordinateZIndex, coordinateXIndex);
-            return tileCoordinate;
         }
     }
 
@@ -109,43 +79,39 @@ public class WorldGeneration : MonoBehaviour
     {
         // generate a noise map using Perlin Noise
         float[,] objectsMap = NoiseMapGeneration.GenerateNoiseMap(levelDepth, levelWidth, levelScale, 0, 0, this.waves, random.Next(0, 1000000));
-        for (int zIndex = 0; zIndex < levelDepth; zIndex++)
-        {
-            for (int xIndex = 0; xIndex < levelWidth; xIndex++)
-            {
-                // convert from Level Coordinate System to Tile Coordinate System and retrieve the corresponding TileData
-                TileCoordinate tileCoordinate = worldData.ConvertToTileCoordinate(zIndex, xIndex);
-                TileGeneration.TileData tileData = worldData.tilesData[tileCoordinate.tileZIndex, tileCoordinate.tileXIndex];
 
-                float objectValue = objectsMap[zIndex, xIndex];
-                // compares the current noise value to the neighbor ones
+        int iterationDelta = 2 * neighborRadius + 1;
+        for (int zIndex = 0; zIndex < levelDepth; zIndex += iterationDelta)
+        {
+            for (int xIndex = 0; xIndex < levelWidth; xIndex += iterationDelta)
+            {
                 int neighborZBegin = (int)Mathf.Max(0, zIndex - neighborRadius);
-                int neighborZEnd = (int)Mathf.Min(levelDepth - 1, zIndex + neighborRadius);
                 int neighborXBegin = (int)Mathf.Max(0, xIndex - neighborRadius);
+                int neighborZEnd = (int)Mathf.Min(levelDepth - 1, zIndex + neighborRadius);
                 int neighborXEnd = (int)Mathf.Min(levelWidth - 1, xIndex + neighborRadius);
                 float maxValue = 0f;
-                for (int neighborZ = neighborZBegin; neighborZ <= neighborZEnd; neighborZ++)
+                int maxZ = zIndex, maxX = xIndex;
+                for (int neighborZ = neighborZBegin; neighborZ <= neighborZEnd; ++neighborZ)
                 {
-                    for (int neighborX = neighborXBegin; neighborX <= neighborXEnd; neighborX++)
+                    for (int neighborX = neighborXBegin; neighborX <= neighborXEnd; ++neighborX)
                     {
                         float neighborValue = objectsMap[neighborZ, neighborX];
                         // saves the maximum noise value in the radius
                         if (neighborValue >= maxValue)
                         {
+                            maxZ = neighborZ;
+                            maxX = neighborX;
                             maxValue = neighborValue;
                         }
                     }
                 }
-                // if the current object noise value is the maximum one, place an object in this location
-                if (objectValue == maxValue)
-                {
-                    Vector3 position = new Vector3(xIndex * distanceBetweenVertices, 1000, zIndex * distanceBetweenVertices); // finding the object's future location using raycast
 
-                    if (Physics.Raycast(position, Vector3.down, out RaycastHit hit, float.MaxValue, (1 << 6))) // 6 is the surface layer, so here we will only find hits with the surface layer
-                    {
-                        GameObject obj = Instantiate(objectsList[random.Next(0, objectsList.Length)], hit.point, Quaternion.identity);
-                        obj.transform.localScale = Vector3.one * 0.5f;
-                    }
+                Vector3 position = new Vector3(maxX * distanceBetweenVertices, 1000, maxZ * distanceBetweenVertices); // finding the object's future location using raycast
+
+                if (Physics.Raycast(position, Vector3.down, out RaycastHit hit, float.MaxValue, (1 << 6))) // 6 is the surface layer, so here we will only find hits with the surface layer
+                {
+                    GameObject obj = Instantiate(objectsList[random.Next(0, objectsList.Length)], hit.point, Quaternion.identity);
+                    obj.transform.localScale = Vector3.one * 0.5f;
                 }
             }
         }
@@ -171,10 +137,10 @@ public class WorldGeneration : MonoBehaviour
 
     public void GenerateGameObjects(int levelDepth, int levelWidth, float levelScale, float distanceBetweenVertices)
     {
-        GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 3, treePrefabs);
-        GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 3, rockPrefabs);
+        GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 7, treePrefabs);
+        GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 7, rockPrefabs);
         GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 10, ironPrefabs);
-        GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 20, goldPrefabs);
+        GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 18, goldPrefabs);
         GenerateObjects(levelDepth, levelWidth, levelScale, distanceBetweenVertices, 30, diamondPrefabs);
     }
 
