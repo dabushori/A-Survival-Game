008904764d806984904df5103b4903f9e7eec439
Yorai <unconfigured@null.spigotmc.org>
2022-04-26 16:46:53 +0300

    Changed that you cant eat with 100 health

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -151,8 +151,9 @@ public class PlayerMovements : MonoBehaviour
     public CraftingMenuInitializer craftingMenuInitializer;
     public void Use()
     {
-        if (Physics.Raycast(gameObject.transform.position, Camera.main.transform.forward, out RaycastHit hit, MINING_DISTANCE, DISTRUCTIBLE_LAYER, QueryTriggerInteraction.Collide)) {
-           if (hit.transform.gameObject.TryGetComponent<RecipeCraftingMenuItem>(out RecipeCraftingMenuItem menuItem))
+        if (Physics.Raycast(gameObject.transform.position, Camera.main.transform.forward, out RaycastHit hit, MINING_DISTANCE, DISTRUCTIBLE_LAYER, QueryTriggerInteraction.Collide))
+        {
+            if (hit.transform.gameObject.TryGetComponent<RecipeCraftingMenuItem>(out RecipeCraftingMenuItem menuItem))
             {
                 switch (menuItem.menuType)
                 {
@@ -168,6 +169,7 @@ public class PlayerMovements : MonoBehaviour
                 }
                 ToggleInventory();
             }
+            return;
         }
         // use logic
         Item currentItem = inventory.ChosenItem;
@@ -192,8 +194,11 @@ public class PlayerMovements : MonoBehaviour
             else if (currentItem.job == Jobs.FOOD && canEat)
             {
                 canEat = false;
-                playerHealth.AddHealth(currentItem.hpBonus);
-                inventory.RemoveFromInventory(currentItem, 1);
+                if (!(playerHealth.Health == 100))
+                {
+                    playerHealth.AddHealth(currentItem.hpBonus);
+                    inventory.RemoveFromInventory(currentItem, 1);
+                }
                 Invoke(nameof(ResetCanEat), EATING_TIME);
             }
         }
